d98c4913093899f720bc9ae545c533344de3939a
Alexey <komandanto666@gmail.com>
2025-04-09 10:31:48 +0300

    task 2, add redis in app

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, jsonify
+import redis
+import os
 
 app = Flask(__name__)
+
+redis_host = os.getenv("REDIS_HOST", "redis")
+redis_client = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
 @app.route("/ping")
 def ping():
     return jsonify({"status": "ok"})
+
+@app.route("/count")
+def count():
+    count = redis_client.incr("visits")
+    return jsonify({"visits": count})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
